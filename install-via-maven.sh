a5d3feaeac4dc9d5317aa95ba32ce3b82ac75eda
djoumbou <djoumbou@ualberta.ca>
2019-11-26 23:06:05 -0500

    Updated the install-via-maven.sh script - added root_dir variable

--- install-via-maven.sh
+++ install-via-maven.sh
@@ -1,5 +1,7 @@
 #/usr/bin/bash
 
+root_dir=$(readlink -f "$(dirname $0)")
+echo $root_dir 
 
 #Cleaning
 echo "Cleaning..."
@@ -7,11 +9,12 @@ mvn clean
 
 #Adding phaseIIFilter
 echo -e "\n\nAdding Phase II Filter JAR..."
-mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file -Dfile=/home/yandj/Projects/Software/metabolite_id/biotransformer/lib/phaseIIFilter-1.0.1.jar -DgroupId=djoumbou -DartifactId=phaseIIFilter -Dversion=1.0.1 -Dpackaging=jar
+mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file -Dfile="${root_dir}/lib/phaseIIFilter-1.0.1.jar" -DgroupId=djoumbou -DartifactId=phaseIIFilter -Dversion=1.0.1 -Dpackaging=jar
+
 
 #Adding Cypreact
 echo -e "\n\nAdding CypReact JAR..."
-mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file -Dfile=/home/yandj/Projects/Software/metabolite_id/biotransformer/lib/cypreact.jar -DgroupId=wishartlab -DartifactId=cypreact -Dversion=1.0.0-SNAPSHOT -Dpackaging=jar
+mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file -Dfile="${root_dir}/lib/cypreact.jar" -DgroupId=wishartlab -DartifactId=cypreact -Dversion=1.0.0-SNAPSHOT -Dpackaging=jar
 
 #Building package
 echo -e "\n\nBuilding Package..." 
